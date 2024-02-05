3bc59c5fdb4c106b76251654ffa37049d03ac08d
Sarvang Dave <sarvang00@gmail.com>
2024-02-04 23:02:40 -0400

    Login form validation

--- simfin-frontend/src/app/users/login/login.component.ts
+++ simfin-frontend/src/app/users/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // Making a reactive form
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  })
 
+  submitLoginForm() {
+    // Login service integration
+    // function placeholder below
+    alert("Username: " + this.loginForm.value.username + "\nPassword: " + this.loginForm.value.password)
+  }
 }
