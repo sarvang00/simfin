4fdd337d51850f7ffa2094bfce4d35d7f622bfeb
Sarvang Dave <sarvang00@gmail.com>
2024-02-04 23:30:38 -0400

    Signup form validation and form setup

--- simfin-frontend/src/app/users/signup/signup.component.ts
+++ simfin-frontend/src/app/users/signup/signup.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signUpForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  })
 
+  submitSignUpForm() {
+    // Handle Sign Up form submission
+    // Placeholder below
+    alert("Signing up "+this.signUpForm.value.username+" with password "+this.signUpForm.value.password)
+  }
 }
