00560e1574b9bbd84756724bdb236bfac9157575
Sarvang Dave <sarvang00@gmail.com>
2024-02-13 00:05:07 -0400

    Refactoring the code a bit - seperating dashboard components

--- simfin-frontend/src/app/dashboard/dashboard.component.ts
+++ simfin-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [LeftSidebarComponent, RightSidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
